ff906abc5c4b1a807c0ace3e08acf6b8980bb014
aeud <adrien.eudes@gmail.com>
2015-08-04 09:30:42 +0800

    update test scripts

--- src/test/scala/PreparatorTest.scala
+++ src/test/scala/PreparatorTest.scala
@@ -13,8 +13,8 @@ class PreparatorTest
   )
 
   val items = Map(
-    "i0" -> Item(categories = Some(List("c0", "c1"))),
-    "i1" -> Item(categories = None)
+    "i0" -> Item(categories = Some(List("c0", "c1")), account = "default", hidden = false),
+    "i1" -> Item(categories = None, account = "default", hidden = false)
   )
 
   val view = Seq(
