27cc4405418cc4bc92c6a4224a67d671f3c8ff2d
aeud <adrien.eudes@gmail.com>
2015-08-13 14:53:30 +0800

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 key_path="/home/ubuntu/spark.pem"
-nodes="4"
 aws_region="us-east-1"
 aws_zone="us-east-1c"
-nodes="4"
-instance_type="m1.large"
+nodes="3"
+instance_type="r3.large"
+master_instance_type="m1.large"
 aws_vpc="vpc-c1b6faa4"
 aws_subnet="subnet-c51fa79c"
 master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
 
 # Destroy Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
+yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
 
 # Launch Spark cluster
-yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i ${key_path} -s ${nodes} --instance-type=${instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn launch pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i ${key_path} -s ${nodes} --instance-type=${instance_type} --master-instance-type=${master_instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn --no-ganglia launch pio
--- scripts/train.sh
+++ scripts/train.sh
@@ -2,6 +2,8 @@
 
 key_path="/home/ubuntu/spark.pem"
 master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
+driver_memory="20"
+executor_memory="13"
 
 # Go into Scilla folder
 cd /home/ubuntu/scilla
@@ -10,7 +12,7 @@ cd /home/ubuntu/scilla
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -i ${key_path} start pio
 
 # Train the model
-/home/ubuntu/PredictionIO/bin/pio train -- --master ${master_url}
+/home/ubuntu/PredictionIO/bin/pio train -- --master ${master_url} --executor-memory ${executor_memory} --driver-memory ${driver_memory}
 
 # Destroy Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
+yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
