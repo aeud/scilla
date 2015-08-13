8dae784b76f54f8d1373057e99b34d4ceb119401
aeud <adrien.eudes@gmail.com>
2015-08-13 14:18:08 +0800

    updated script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,15 @@
  #!/bin/bash
 
+nodes="4"
+aws_region="us-east-1"
+aws_zone="us-east-1c"
+nodes="4"
+instance_type="m1.large"
+aws_vpc="vpc-c1b6faa4"
+aws_subnet="subnet-c51fa79c"
+
 # Destroy Spark cluster
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
 
 # Launch Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i /home/ubuntu/spark.pem -s 1 --instance-type=m1.large --copy-aws-credentials --vpc-id=vpc-c1b6faa4 --subnet-id=subnet-c51fa79c --region=us-east-1 --zone=us-east-1c --hadoop-major-version=yarn launch pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i /home/ubuntu/spark.pem -s ${nodes} --instance-type=${instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn launch pio
