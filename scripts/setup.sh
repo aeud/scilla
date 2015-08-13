670362499b60d732be7664357c4a3442751448d1
aeud <adrien.eudes@gmail.com>
2015-08-13 14:44:44 +0800

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,7 @@ nodes="4"
 instance_type="m1.large"
 aws_vpc="vpc-c1b6faa4"
 aws_subnet="subnet-c51fa79c"
+master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
 
 # Destroy Spark cluster
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 key_path="/home/ubuntu/spark.pem"
+master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
 
 # Go into Scilla folder
 cd /home/ubuntu/scilla
@@ -9,7 +10,7 @@ cd /home/ubuntu/scilla
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -i ${key_path} start pio
 
 # Train the model
-/home/ubuntu/PredictionIO/bin/pio train
+/home/ubuntu/PredictionIO/bin/pio train -- --master ${master_url}
 
 # Destroy Spark cluster
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
