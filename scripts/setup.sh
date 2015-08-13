42f6c8d4273f384a8b18314d1f856aaf0615edbb
aeud <adrien.eudes@gmail.com>
2015-08-13 19:08:05 +0800

    New stack

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,9 @@
 key_path="/home/ubuntu/spark.pem"
 aws_region="us-east-1"
 aws_zone="us-east-1c"
-nodes="10"
-instance_type="m1.large"
+nodes="3"
+instance_type="m4.xlarge"
+master_instance_type="m4.large"
 aws_vpc="vpc-c1b6faa4"
 aws_subnet="subnet-c51fa79c"
 master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
@@ -13,7 +14,7 @@ master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
 yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
 
 # Launch Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i ${key_path} -s ${nodes} --instance-type=${instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn --no-ganglia launch pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i ${key_path} -s ${nodes} -t ${instance_type} -m ${master_instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn --no-ganglia launch pio
 
 # Stop the cluster
-#yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
+yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
--- scripts/train.sh
+++ scripts/train.sh
@@ -9,13 +9,10 @@ executor_memory="6"
 cd /home/ubuntu/scilla
 
 # Launch Spark cluster
-#/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -i ${key_path} start pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -i ${key_path} start pio
 
 # Train the model
 /home/ubuntu/PredictionIO/bin/pio train -- --master ${master_url} --executor-memory ${executor_memory} --driver-memory ${driver_memory}
 
 # Destroy Spark cluster
-#yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
-
-
-#JAVA_OPTS=-Xmx16g /home/ubuntu/PredictionIO/bin/pio train -- --master spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077 --executor-memory 13 --driver-memory 20
+yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 stop pio
