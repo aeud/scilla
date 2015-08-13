a917a17f3ba781b475c56d2f7d36336c6704fa98
aeud <adrien.eudes@gmail.com>
2015-08-13 19:36:27 +0800

    New stack

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,7 +8,6 @@ instance_type="m3.xlarge"
 master_instance_type="m3.medium"
 aws_vpc="vpc-c1b6faa4"
 aws_subnet="subnet-c51fa79c"
-master_url="spark://ec2-52-3-89-72.compute-1.amazonaws.com:7077"
 
 # Destroy Spark cluster
 yes | /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
