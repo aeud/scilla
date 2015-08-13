ee18aaa8c9de0dead436371f944b4b92601aa6ef
aeud <adrien.eudes@gmail.com>
2015-08-13 14:38:34 +0800

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,6 @@
- #!/bin/bash
+#!/bin/bash
 
+key_path="/home/ubuntu/spark.pem"
 nodes="4"
 aws_region="us-east-1"
 aws_zone="us-east-1c"
@@ -12,4 +13,4 @@ aws_subnet="subnet-c51fa79c"
 /home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 destroy pio
 
 # Launch Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i /home/ubuntu/spark.pem -s ${nodes} --instance-type=${instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn launch pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -k spark -i ${key_path} -s ${nodes} --instance-type=${instance_type} --copy-aws-credentials --vpc-id=${aws_vpc} --subnet-id=${aws_subnet} --region=${aws_region} --zone=${aws_zone} --hadoop-major-version=yarn launch pio
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,12 @@
- #!/bin/bash
+#!/bin/bash
+
+key_path="/home/ubuntu/spark.pem"
 
 # Go into Scilla folder
 cd /home/ubuntu/scilla
 
 # Launch Spark cluster
-/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 start pio
+/home/ubuntu/PredictionIO/vendors/spark-1.4.1/ec2/spark-ec2 -i ${key_path} start pio
 
 # Train the model
 /home/ubuntu/PredictionIO/bin/pio train
