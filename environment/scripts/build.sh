d8d59376dd38c313345f89e26a1aaa44862ac705
aeud <adrien.eudes@gmail.com>
2015-08-18 14:34:28 +0800

    Try to loop :)

--- environment/scripts/build.sh
+++ environment/scripts/build.sh
@@ -3,18 +3,20 @@
 # Set up Scilla
 cd /home/ubuntu
 rm -rf scilla
-git clone git@github.com:project-aprils/scilla.git
+yes | git clone git@github.com:project-aprils/scilla.git
 cd scilla
 /home/ubuntu/PredictionIO/bin/pio build --verbose
 
 # Configure proxy
 sudo cp /home/ubuntu/scilla/environment/nginx/scilla /etc/nginx/sites-available/
+sudo rm /etc/nginx/sites-enabled/scilla
 sudo ln -s /etc/nginx/sites-available/scilla /etc/nginx/sites-enabled/scilla
 sudo service nginx restart
 
 # Set up Scilla as a service
-sudo cp /home/ubuntu/scilla/environment/scripts/service.sh /etc/init.d/scilla
-sudo update-rc.d scilla defaults
+# sudo cp /home/ubuntu/scilla/environment/scripts/service.sh /etc/init.d/scilla
+# sudo update-rc.d -f scilla remove
+# sudo update-rc.d scilla defaults
 
 # Launch Scilla
 sudo service scilla deploy
