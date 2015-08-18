cf207b4d32986905d18c8de329144df5d29bc4b5
aeud <adrien.eudes@gmail.com>
2015-08-18 14:04:02 +0800

    fixed some details

--- environment/scripts/deploy.sh
+++ environment/scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd /home/ubuntu/scilla
-sudo /home/ubuntu/PredictionIO/bin/pio deploy --port 8001 -- --driver-memory 6g >> /var/log/pio.log &>> pio-err.log 
+/home/ubuntu/PredictionIO/bin/pio deploy --port 8001 --engine-id 709vsYeLDjbQ13lAB718UdpxlHvD6jrG --engine-version a45f3aacfb7e261f4f7707e8641f1aae6c000651 -- --driver-memory 6g >> /home/ubuntu/log/pio.log &>> /home/ubuntu/log/pio-err.log 
--- environment/scripts/ec2.sh
+++ environment/scripts/ec2.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-sudo apt-get update && yes | sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install build-essential git nginx
 
-cd
-
-sudo apt-get install build-essentials git nginx
-
+# Set up PIO
 bash -c "$(curl -s https://install.prediction.io/install.sh)"
 
-sudo cp /home/ubuntu/scilla/environment/nginx/scilla /etc/nginx/sites-available
-sudo ln -s /etc/nginx/sites-available/scilla /etc/nginx/sites-enabled/scilla
+# Set up Scilla
+cd /home/ubuntu
+git clone git@github.com:project-aprils/scilla.git
+cd scilla
+/home/ubuntu/PredictionIO/bin/pio build --verbose
 
+# Configure proxy
+sudo cp /home/ubuntu/scilla/environment/nginx/scilla /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/scilla /etc/nginx/sites-enabled/scilla
 sudo service nginx restart
 
-cd /home/ubuntu/scilla
-pio deploy --port 8001 -- --driver-memory 4g
+# Set up Scilla as a service
+sudo cp /home/ubuntu/scilla/environment/scripts/service.sh /etc/init.d/scilla
+
+# Launch Scilla
+sudo service scilla start
