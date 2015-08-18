65e4b74ad2653357964cc0e69052ed87972cd85a
aeud <adrien.eudes@gmail.com>
2015-08-18 14:27:16 +0800

    Try to loop :)

--- environment/scripts/ec2.sh
+++ environment/scripts/ec2.sh
@@ -8,6 +8,7 @@ bash -c "$(curl -s https://install.prediction.io/install.sh)"
 
 # Set up Scilla
 cd /home/ubuntu
+rm -rf scilla
 git clone git@github.com:project-aprils/scilla.git
 cd scilla
 /home/ubuntu/PredictionIO/bin/pio build --verbose
@@ -19,6 +20,7 @@ sudo service nginx restart
 
 # Set up Scilla as a service
 sudo cp /home/ubuntu/scilla/environment/scripts/service.sh /etc/init.d/scilla
+sudo update-rc.d scilla defaults
 
 # Launch Scilla
 sudo service scilla start
--- environment/scripts/service.sh
+++ environment/scripts/service.sh
@@ -2,7 +2,10 @@
 
 case "$1" in
     start)
-        bash /home/ubuntu/scilla/environment/scripts/deploy.sh >> /home/ubuntu/log/pio.log &>> /home/ubuntu/log/pio-err.log
+        bash /home/ubuntu/scilla/environment/scripts/build.sh #>> /home/ubuntu/log/pio.log &>> /home/ubuntu/log/pio-err.log
+        ;;
+    deploy)
+        bash /home/ubuntu/scilla/environment/scripts/deploy.sh #>> /home/ubuntu/log/pio.log &>> /home/ubuntu/log/pio-err.log
         ;;
 esac
 
